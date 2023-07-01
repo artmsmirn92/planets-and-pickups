067cc0387b942a61e86c041301da00bf9ab9429c
Artem Smirnov <artmsmirn92@gmail.com>
2023-07-01 04:34:50 +0300

    InputContoller

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,75 @@
+﻿using Lean.Common;
+using MiniPlanetDefense;
+using UnityEngine;
+using Zenject;
+// ReSharper disable UnassignedGetOnlyAutoProperty
+
+public class InputController : MonoBehaviour
+{
+    #region nonpublic members
+
+    private bool m_ProceedPlayerInput;
+
+    #endregion
+    
+    #region inject
+
+    [Inject] private Player            Player            { get; }
+    [Inject] private PressKeyToStart   PressKeyToStart   { get; }
+    [Inject] private PressKeyToRestart PressKeyToRestart { get; }
+    
+    #endregion
+    
+    #region engine methods
+
+    private void Awake()
+    {
+        Player.Death              += OnPlayerDeath;
+        PressKeyToStart.Start     += OnLevelStart;
+        PressKeyToRestart.Restart += OnLevelRestart;
+    }
+    
+    private void Update()
+    {
+        if (!m_ProceedPlayerInput)
+            return;
+        ProceedJump();
+        ProceedMovement();
+    }
+
+    #endregion
+
+    #region nonpublic methods
+
+    private void OnLevelStart()
+    {
+        m_ProceedPlayerInput = true;
+    }
+
+    private void OnLevelRestart()
+    {
+        m_ProceedPlayerInput = true;
+    }
+
+    
+    private void OnPlayerDeath()
+    {
+        m_ProceedPlayerInput = true;
+    }
+    
+    private void ProceedJump()
+    {
+        if (LeanInput.GetDown(KeyCode.Space))
+            Player.InvokeJump();
+    }
+
+    private void ProceedMovement()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
+        var dir = new Vector2(dirX, dirY);
+        Player.SetMoveDir(dir);
+    }
+
+    #endregion
+}
